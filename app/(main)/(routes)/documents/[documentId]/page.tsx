15de783a5f386926ac2224c5c26158ce00ae5cb9
DorijanH <dhaspl@gmail.com>
2023-10-16 11:36:15 +0200

    Document detail page loading skeletons

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -4,6 +4,7 @@ import { useQuery } from 'convex/react';
 
 import { Id } from '@/convex/_generated/dataModel';
 import { api } from '@/convex/_generated/api';
+import { Skeleton } from '@/components/ui/skeleton';
 import Toolbar from '@/components/toolbar';
 import Cover from '@/components/cover';
 
@@ -20,7 +21,15 @@ export default function DocumentIdPage({ params: { documentId } }: DocumentIdPag
   if (document === undefined) {
     return (
       <div>
-        Loading...
+        <Cover.Skeleton />
+        <div className="mx-auto mt-10 md:max-w-3xl lg:max-w-4xl">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
+        </div>
       </div>
     );
   }
