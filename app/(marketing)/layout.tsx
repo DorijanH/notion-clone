383c20e9a8492d0d1d0d9694dcda024e9908d1e1
DorijanH <dhaspl@gmail.com>
2023-10-16 14:51:12 +0200

    Background color fix

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,10 +2,10 @@ import Navbar from './_components/navbar';
 
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-full dark:bg-[#1F1F1F]">
+    <div className="h-full dark:bg-[#1F1F1F]">
       <Navbar />
 
-      <main className="min-h-full pt-40">
+      <main className="h-full pt-40">
         {children}
       </main>
     </div>
